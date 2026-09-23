3d041598900952dbe8d91349a3a4bf20e1a0d403
Sandeep Reddy <muchasandeepreddy176@gmail.com>
2026-09-23 16:10:01 +0530

    Remove duplicate backend project

--- backend/serviconnect-backend-main/src/main/java/com/practice/repository/ServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.practice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.practice.entity.Service;
-
-public interface ServiceRepository extends JpaRepository<Service, Long> {
-
-}
